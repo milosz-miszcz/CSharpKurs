3f33494f8688cb6b22141ec90095e9346974dec0
Miłosz Miszczyk <miloszxpompa08@gmail.com>
2020-10-17 12:02:05 +0200

    Pierszy program

---
+++ Hello/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; // Musi byc duza litera
+
+namespace Hello // Tytul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* string name = args[0];
+            Console.WriteLine("Witaj, " + name); */
+
+            foreach (var item in args)
+            {
+                Console.WriteLine("Witaj," + item);
+            }
+
+        }
+    }
+}
